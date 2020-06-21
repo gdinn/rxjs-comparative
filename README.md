74d1dd6c6fa31a0e5249678575b239de98c3dff7
Igor <godinho.igor@gmail.com>
2020-06-21 17:27:56 -0300

    Changes on the merge
    Maintained:
    - Promise approach (very nice)
    - Obs approach on loading dismissal
    - Sub sink (very interesting)
    
    Not used
    - Utils refactor (my approach to store and delete the alert and loading elements was intentional)
    - Obs approach on present loadings and using the refactored utils loading implementation (it wasnt used because on the case of the service responding instantly, the loading would be dismissed before it was presented)
    
    Inspired:
    - Reorganization on the flux of the obs approach
    - Reorganization on the flux of the promise approach to be as similar as possible of obs flux
    - Redesigning the alert and loading call on the promise approach (to match the obs approach)
    
    Not related:
    - shortened all the comments and logs
    - added more details on the README to reflect the current stage of the demonstration.

--- README.md
+++ README.md
@@ -1,26 +1,54 @@
-# Rxjs and pure JS approaches on concurrent async calls.
-This repository showcases the use of different aproaches on concurrent async calls. The frameworks in use are Ionic and Angular.
+# RxJS and pure JS comparison.
 
-The goal is to present a loading, get some data, treat possible errors and dismiss the loading without using logic to block the flux.
-It is indeed a easy task, but
-if the scenario is more complex we start to observe some interesting issues.
+This repository showcases the use of different aproaches on concurrent asynchronous calls. The frameworks in use are Ionic and Angular.
 
-Async/await approach:
-- Limitation of presenting a loading and being unable to dismiss it after concurrent calls. **
-  - In other words, we cant block the flux and wait for simultaneous calls end.
-- Callback hell (http://callbackhell.com/).
+The goal is to present a loading, get some data, treat the result, treat possible errors and dismiss the loading without using logic to block the flux.
+
+What will happens is that both aproaches are similar in the flux, but very different on handling the data.
+
+The data handling on RxJS is more granular than the pure JS, but requires a steeper work curve.
+
+When working with angular, learn RxJS is very useful because angular use several modules that works with observables.
+
+## Async/await
+
+**Pros**
+
+* Simpler to learn
+* Most of JS libs supports promises
+
+**Cons**
+
 - Sometimes the UI doesnt get updated (https://github.com/angular/angular/issues/7381)
+- Difficult to control more complex fluxes with cancelation, for example (only example in mind that does not envolve streams)
+- Cannot handle streams
 
-RxJS approach:
-- Promise thingies must become observables, and for this reason a refactoring workload is required.
-- Learn the non obvious RxJS way of doing things. 
+## RxJS approach:
 
-There are drawbacks on both aproaches but once you got RxJS covered, the result is better since you can do the most varied tasks
-with this library.
+**Pros**
 
-** Of course we can dismiss the loading with some logic, but that is not the point.
+* Can handle streams in a bunch of ways (skipping, debouncing, buffering, etc)
+* Low impact considering the benefit
+* Granular control over the flux
+
+**Cons**
+
+* Promises will have better supported if converted to observables, and therefore a workload is required.
+* Learn the non obvious RxJS way of doing things.
+* Could lead to memory leaks if the streams werent unsubscribed
+
+## Conclusion
+
+There are drawbacks on both aproaches but once you got RxJS covered, it will give you more control over the flux with the possibility to handle streams.
+
+## Interesting links
+
+* https://dev.to/avatsaev/simple-state-management-in-angular-with-only-services-and-rxjs-41p8
+* https://auth0.com/blog/javascript-promises-vs-rxjs-observables/
+* https://medium.com/javascript-everyday/javascript-theory-promise-vs-observable-d3087bc1239a
 
 ## How to test?
+
 ```
 git clone https://github.com/gdinn/rxjs-comparative
 cd rxjs-comparative
@@ -33,6 +61,3 @@ After serving the application, just click on one button at the time to observe t
 If there are another way to do things that are more efficient, feel free to email me, or pull request into this repository.
 
 Thanks!
-
-
-
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,7 +4,6 @@ import { BooksRxjsService } from 'src/services/books.rxjs.service';
 import { Observable, concat, merge, of } from 'rxjs';
 import { shareReplay, map, catchError, tap } from 'rxjs/operators';
 import { UtilsService } from 'src/services/utils.service';
-import { LoadingController, AlertController } from '@ionic/angular';
 
 import { SubSink } from 'subsink';
 
@@ -29,9 +28,7 @@ export class HomePage implements OnDestroy {
   constructor(
     private bookPromiseService: BooksPromiseService,
     private bookRxjsService: BooksRxjsService,
-    private utilsService: UtilsService,
-    private loadingCtrl: LoadingController,
-    private alertCtrl: AlertController
+    private utilsService: UtilsService
   ) {}
 
   ngOnDestroy() {
@@ -41,106 +38,83 @@ export class HomePage implements OnDestroy {
   async refreshPromises() {
     this.promisesActivated = true;
     this.rxjsActivated = false;
-    const loading = await this.loadingCtrl.create({ message: 'Please wait...' });
-    await loading.present();
-    try {
-      // Agrupar promises a serem resolvidas
-      await Promise.all([this.getPromiseBooks(), this.getPromiseLibraries()]);
-    } catch (e) {
-      // N precisa alocar variável
-      await this.alertCtrl
-        .create({
-          message: e,
-          buttons: ['Ok'],
-        })
-        .then((res) => res.present());
+    try {      
+      await this.utilsService.presentLoading("refreshPromise").toPromise()
+      await this.getPromiseData()
+      await this.utilsService.dismissLoading("refreshPromise").toPromise()
+    } catch (err) {      
+      await this.utilsService.dismissLoading("refreshPromise").toPromise()
+      await this.utilsService.presentAlert("refreshPromise", {
+        message: err,
+        buttons: ['Ok'],
+      }).toPromise()
+    }    
+  }
+
+
+  private getPromiseData() {
+    let promiseLibraries = () => {
+      return this.bookPromiseService.getLibraries().then((res) => (this.libraries = res.data));
     }
-    loading.dismiss();
-  }
 
-  private getPromiseBooks() {
-    return this.bookPromiseService.getBooks().then((res) => {
-      this.available = res.data.filter((book: any) => book.status == 'Available');
-      this.unavailable = res.data.filter((book: any) => book.status == 'Unavailable');
-    });
-  }
+    let promiseBooks = () => {
+      return this.bookPromiseService.getBooks().then((res) => {
+        this.available = res.data.filter((book: any) => book.status == 'Available');
+        this.unavailable = res.data.filter((book: any) => book.status == 'Unavailable');
+      });
+    }
+    
+    return Promise.all([ //call simultaneously
+      promiseBooks(), 
+      promiseLibraries()
+    ])
 
-  private getPromiseLibraries() {
-    return this.bookPromiseService.getLibraries().then((res) => (this.libraries = res.data));
   }
 
   refreshRxjs() {
     this.promisesActivated = false;
-    this.rxjsActivated = true;
-
-    this.libraries$ = this.bookRxjsService.getLibraries();
-
-    // exemplo 1
-    // this.utilsService.presentL(), // fora do strem
-    //   merge(this.getObsBooks(), this.libraries$)
-    //     .pipe(
-    //       catchError((err) => {
-    //         this.utilsService.presetA({
-    //           message: err,
-    //           buttons: ['Ok'],
-    //         });
-    //         return null;
-    //       }),
-    //       finalize(() => {
-    //         this.utilsService.dismissL();
-    //       })
-    //     )
-    //     .subscribe();
-
-    // exemplo 2
-    this.utilsService.presentL(),
-      this.subs$.add(
-        merge(this.getObsBooks(), this.libraries$)
-          .pipe(
-            tap(() => this.utilsService.presentL()), // dentro do fluxo como sideeffect),
-            catchError((err) => {
-              this.utilsService.dismissL();
-              this.utilsService.presetA({
-                message: err,
-                buttons: ['Ok'],
-              });
-              return null;
+    this.rxjsActivated = true;    
+    this.subs$.add(
+      concat( //call in order, one at time
+        this.utilsService.presentLoading("refreshRxjs"),
+        this.getObsData(),
+        this.utilsService.dismissLoading("refreshRxjs")            
+      )
+      .pipe(
+        catchError((err) => {
+          return concat( //concat prevent messing the UI
+            this.utilsService.dismissLoading("refreshRxjs"),
+            this.utilsService.presentAlert("refreshRxjs", {
+              message: err,
+              buttons: ['Ok'],
             })
-            // Finalize só é chamado em alguns contexto conde o observable é completado.
-            // Por example router.queryParams não tem ponto de parada, logo está função nunca será executada.
-            // Caso queira usar o finalize em contextos de erro é importante usar sua chamada antes do catchError dentro do pipe.
-
-            // finalize(() => {
-            //   this.utilsService.dismissL();
-            // })
-          )
-          .subscribe(null, null, () => {
-            // chama o q seria chamado  no finalize no completo do subscrive.
-            // Acontece que as vezes queremos que algo assim aconteça em um fluxo chamado diretamente pelo HTML com um | async
-            // neste caso podemos usar:
-            // 1. Finelize antes do catch error, se tivermos certeza de q ele será finalizado
-            // 2. Tap após lógica do fluxo
-            // 3. Chamar a operação dentro do fluxo de sucesso (podendo ser um map) e dentro do fluxo de erro.
-            // Neste exemplo utilizado no complete, aqui mesmo, e no catchError.
-            return this.utilsService.dismissL();
-          })
-      );
-  }
-
-  private getObsBooks() {
-    return this.bookRxjsService.getBooks().pipe(
-      shareReplay(),
-      map((books: any[]) => {
-        this.available$ = of(books.filter((book) => book.status == 'Available'));
-        this.unavailable$ = of(books.filter((book) => book.status == 'Unavailable'));
-      })
+          )                
+        })
+      )
+      .subscribe()        
     );
+    //Could use finalize, but obs need to finalize.
   }
 
-  private test() {
-    const c = concat(of('1'), of('2'), of('3'));
-    const m = merge(of('1'), of('2'), of('3'));
-    c.subscribe(console.log);
-    c.subscribe(console.log);
+  private getObsData(): Observable<any>{    
+    let obsLibraries = () => { //define obs
+      this.libraries$ = this.bookRxjsService.getLibraries()
+      return this.libraries$      
+    }
+
+    let obsBooks = () => { //define obs and organize data
+      return this.bookRxjsService.getBooks().pipe(
+        shareReplay(),
+        map((books: any[]) => {
+          this.available$ = of(books.filter((book) => book.status == 'Available'));
+          this.unavailable$ = of(books.filter((book) => book.status == 'Unavailable'));
+        })
+      );
+    }
+
+    return merge( //call simultaneously
+      obsBooks(), 
+      obsLibraries()
+    )
   }
 }
